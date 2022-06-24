4b83aa75c08d6ed4a56431e831f761f5bd78b797
Shamsheer Ahamed <shamsheer.ahamed@ninjavan.co>
2022-06-24 20:09:56 +0800

    Init

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import { SessionProvider } from "next-auth/react";
+import { AppProps } from "next/app";
+import '../styles/globals.css'
+import 'tailwindcss/tailwind.css';
+
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+export default App;
